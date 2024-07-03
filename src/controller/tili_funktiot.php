84b90037462c168b9b29c8a92ad6f725664c4224
Leena Kevätkylä <p85068@edu.sasky.fi>
2024-07-03 16:29:30 +0300

    Lomaketietojen tarkistaminen

---
+++ src/controller/tili_funktiot.php
@@ -0,0 +1,60 @@
+<?php
+
+function lisaaTili($formdata) {
+    require_once(MODEL_DIR . 'henk_funktiot.php');
+    $error =[];
+
+    if (!isset($formdata['nimi']) || !$formdata['nimi']) {
+        $error['nimi'] = "Syötä nimi.";
+    } else {
+        if (!preg_match("/^[- '\p{L}]+$/u", $formdata["nimi"])) {
+            $error['nimi'] = "Syötä nimi ilman erikoismerkkejä.";
+        }
+    }
+
+    if (!isset($formdata['email']) || !$formdata['email']) {
+        $error['email'] = "Syötä sähköpostiosoite.";
+    } else {
+        if (!filter_var($formdata['email'], FILTER_VALIDATE_EMAIL)) {
+            $error = "Sähköpostiosoite on virheellisessä muodossa.";
+        }
+    }
+
+    if (isset($formdata['salasana1']) && $formdata['salasana1'] &&
+        isset($formdata['salasana2']) && $formdata['salsana2']) {
+            if ($formdata['salasana1'] != $formdata['salasana2']) {
+                $error['salasana'] = "Salasanat eivät täsmää.";
+            }
+        } else {
+            $error['salasana'] = "Syötä salasana kahteen kertaan.";
+        }
+    
+    if (!$error) {
+        $nimi = $formdata['nimi'];
+        $email = $formdata['email'];
+        $salasana = password_hash($formdata['salasana1'], PASSWORD_DEFAULT);
+
+        $idhenkilo = lisaaHenkilo($nimi, $email, $salasana);
+
+        if ($idhenkilo) {
+            return [
+                "status" => 200,
+                "id" => $idhenkilo,
+                "data" => $formdata
+            ];
+        } else {
+            return [
+                "status" => 500,
+                "data" => $formdata,
+            ];
+        }
+    } else {
+        return [
+            "status" => 400,
+            "data" => $formdata,
+            "error" => $error
+        ];
+    }
+}
+
+?>
