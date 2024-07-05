02c15b5326f3d1e640b28fc2677a49ccf06ed311
Leena Kevätkylä <p85068@edu.sasky.fi>
2024-07-05 11:38:45 +0300

    Ilmoittautuminen ja peruminen kutsukäsittelijään

--- public/index.php
+++ public/index.php
@@ -77,6 +77,32 @@ switch ($request) {
         header("Location: " . $config['urls']['baseUrl']);
         break;
 
+    case '/ilmoittaudu':
+        if ($_GET['id']) {
+            require_once MODEL_DIR . 'ilmo_funktiot.php';
+            $idtapahtuma = $_GET['id'];
+            if ($loggeduser) {
+                lisaaIlmoittautuminen($loggeduser['idhenkilo'], $idtapahtuma);
+            }
+            header("Location: tapahtuma?id=$idtapahtuma");
+        } else {
+            header("Location: tapahtumat");
+        }
+        break;
+
+    case '/peru':
+        if ($_GET['id']) {
+            require_once MODEL_DIR . 'ilmo_funktiot.php';
+            $idtapahtuma = $_GET['id'];
+            if ($loggeduser) {
+                poistaIlmoittautuminen($loggeduser['idhenkilo'],$idtapahtuma);
+            }
+            header("Location: tapahtuma?id=$idtapahtuma");
+        } else {
+            header("Location: tapahtumat");
+        }
+        break;
+
     default:
         echo $templates->render('notfound');
 }
