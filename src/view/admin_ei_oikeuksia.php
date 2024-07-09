25fd892016f888975d21f5f1ff61236bfd302ecc
Leena Kevätkylä <p85068@edu.sasky.fi>
2024-07-09 13:11:41 +0300

    Admin-toiminnallisuus poistettu

--- src/view/admin_ei_oikeuksia.php
+++
@@ -1,6 +0,0 @@
-<?php $this->layout('template', ['title' => 'Käyttöoikeudet puuttuvat']) ?>
-
-<h1>Ei käyttöoikeutta</h1>
-
-<p>Sinulla ei ole ylläpitoon tarvittavia käyttöoikeuksia. Voit olla tarvittaessa 
-    yhteydessä sivuston ylläpitäjään.</p>
--- src/view/template.php
+++ src/view/template.php
@@ -7,6 +7,7 @@
     </head>
     <body>
         <header>
+            <!-- Tämä baseurl-juttu vähän kökkö - oma etusivu olis kiva mut sit toi pitäis korjata -->
             <h1><a href="<?=BASEURL?>">Lauantain lukuseura</a></h1>
             
             <!-- Tapahtumat -->
@@ -22,9 +23,6 @@
                     if (isset($_SESSION['user'])) {
                         echo "<div>$_SESSION[user]</div>";
                         echo "<div><a href='logout'>Kirjaudu ulos</a></div>";
-                        if (isset($_SESSION['admin']) && $_SESSION['admin']) {
-                            echo "<div><a href='admin'>Ylläpitosivut</a></div>";
-                        }
                     } else {
                         echo "<div><a href='kirjaudu'>Kirjaudu</a></div>";
                     }
